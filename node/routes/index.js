2000d7b6e0232eddc851fffb1db726e001001834
Andreas Mayer <andy@haellisch-gut.de>
2022-03-12 19:32:40 +0100

    updated response object

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hello Git! This is a change in the 2nd branch.' });
 });
 
 module.exports = router;
